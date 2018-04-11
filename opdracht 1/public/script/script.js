77be68f37209ff34889318047dd1f3a3a7cfd437
Rick712 <rick.buter@gmail.com>
2018-04-11 15:04:36 +0200

    improvements to style

--- opdracht 1/public/script/script.js
+++ opdracht 1/public/script/script.js
@@ -14,5 +14,23 @@
 
     const featuredImage = document.querySelector('.featuredImage')
     featuredImage.style.backgroundImage = 'url("https://i.imgur.com/xkKw5r8.jpg")'
+
+    const allImages = document.querySelectorAll('a'),
+        highlighted = document.querySelector('.highlighted')
+    window.addEventListener('hashchange', function() {
+        highlighted.classList.add('highlightedBlock')
+        allImages.forEach( function (image) {
+            const urlHash = image.href.split("html")[1]
+            if (window.location.hash == urlHash) {
+                const highlightedImage = document.querySelector('.highlightedImage')
+                highlightedImage.setAttribute('src', 'public/images/' + (urlHash.split('#')[1]) + '.jpg')
+            }
+        })
+    })
+
+    const begoneHighlighted = document.querySelector('.begoneHighlighted')
+    begoneHighlighted.addEventListener('click', function() {
+        highlighted.classList.remove('highlightedBlock')
+    })
     
 })()
