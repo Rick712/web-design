069ac8610bdb3fc521c8e06e9cd4b6c56799d4c8
Rick712 <rick.buter@gmail.com>
2018-04-23 15:18:14 +0200

    style almost finished

---
+++ opdracht_2/dist/js/index.js
@@ -0,0 +1,13 @@
+(function(){
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            })
+        })
+    })
+
+    console.log('jha;;adf')
+})()
--- opdracht_2/gulpfile.js
+++ opdracht_2/gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
+
+gulp.task('watch', function () {
+    gulp.watch('style/style.scss', ['sass']);
+})
+
 gulp.task('sass', function(){
     return gulp.src('style/style.scss')
         .pipe(sass())
