975f723d6f8f70b8a6f99d741ee6a3ce01669921
Rick712 <rick.buter@gmail.com>
2018-05-24 12:00:39 +0200

    added alert

--- opdracht_2/dist/js/index.js
+++ opdracht_2/dist/js/index.js
@@ -1,23 +1,30 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+(function() {
+    const home = document.querySelector('.home')
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+    if (home) {
+        
+        const anchor = document.querySelectorAll('a[href^="#"]')
+        anchor.forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
         });
-    });
-});
 
-const studentAlert = document.querySelector('.alert'),
-    alertGone = document.querySelector('.alertGone')
+        const studentAlert = document.querySelector('.alert'),
+            alertGone = document.querySelector('.alertGone')
 
-const alertStudent = function () {
-    console.log('hallo')
-    studentAlert.style.right = '0'
+        const alertStudent = function () {
+            studentAlert.style.right = '0'
 
-    alertGone.addEventListener('click', function() {
-        studentAlert.style.right = 'calc(-30vw - 8rem)'
-    })
-}
+            alertGone.addEventListener('click', function () {
+                studentAlert.style.right = 'calc(-30vw - 8rem)'
+            })
+        }
 
-setTimeout(alertStudent, 1500)
+        setTimeout(alertStudent, 1500)
+    }
+})()
